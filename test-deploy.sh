8dfdfccc3a462132780c4ad642d3b33c50db0d3c
Tyler <tylergrinn@gmail.com>
2020-09-23 09:46:17 -0400

    expanding folder creation logic

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-path=/reports/$1
+if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+  path=/reports/$TRAVIS_PULL_REQUEST_BRANCH
+else
+  path=/reports/$TRAVIS_BRANCH
+fi
+
 rsync --recursive --delete reports tygr@beta.tygr.info:tygr$path/
 echo build-report.html and test-report.html saved to https://beta.tygr.info$path
 echo They will be preserved for 2 weeks
