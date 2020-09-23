eba0b0dca6a8173e879df3eb7d6e0d9bc4a297eb
Tyler <tylergrinn@gmail.com>
2020-09-23 12:19:26 -0400

    moved ssh setup to after installation

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-host=beta.tygr.info
-
 if [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
   path=reports/$TRAVIS_PULL_REQUEST_BRANCH
 else
   path=reports/$TRAVIS_BRANCH
 fi
 
-ssh tygr@$host mkdir -p tygr/$path
-rsync --recursive --delete reports/* tygr@$host:tygr/$path
-echo build-report.html and test-report.html saved to https://$host/$path
+ssh tygr@$HOST mkdir -p tygr/$path
+rsync --recursive --delete reports/* tygr@$HOST:tygr/$path
+echo build-report.html and test-report.html saved to https://$HOST/$path
 echo They will be preserved for 2 weeks
