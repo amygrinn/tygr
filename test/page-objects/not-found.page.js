dba77e7e2c8a2926792d807e682555378e2e53da
Tyler <tylergrinn@gmail.com>
2020-09-25 00:57:54 -0400

    cleaned up test folder

--- test/page-objects/not-found.page.js
+++
@@ -1,9 +0,0 @@
-const Page = require('./page');
-
-class SplashPage extends Page {
-  get header() {
-    return $('h1');
-  }
-}
-
-module.exports = new SplashPage();
--- test/page-objects/splash.page.js
+++ test/page-objects/splash.page.js
@@ -1,10 +1,6 @@
 const Page = require('./page');
 
 class SplashPage extends Page {
-  get header() {
-    return $('h1');
-  }
-
   getLink(href) {
     return $(`a[href='${href}']`);
   }
