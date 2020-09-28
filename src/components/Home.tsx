8bc913a8563933139320e89ddd526801849c9304
Tyler <tylergrinn@gmail.com>
2020-09-28 10:06:32 -0400

    added description

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,5 +4,11 @@ import Page, { PageProps } from './Page';
 type HomeProps = PageProps;
 
 export default (props: HomeProps) => (
-  <Page {...props} name="TyGr Development!" path="/" className="home" />
+  <Page {...props} name="TyGr Development!" path="/" className="home">
+    <p className="description">
+      My name is Tyler Grinn and these are some of my projects. Everything here
+      uses the MIT open source license. In short, you may reuse and modify all
+      code in any open source or commercial context, given no warranty.
+    </p>
+  </Page>
 );
